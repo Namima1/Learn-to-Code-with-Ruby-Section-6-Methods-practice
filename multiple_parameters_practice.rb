f7639df9fcfba656952353a6fdf3cce2f2b17278
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-14 11:02:11 -0400

    Add files via upload

---
+++ multiple_parameters_practice.rb
@@ -0,0 +1,11 @@
+def praise_cat(name, age)
+    #added age as second parameter
+    puts "#{name} is cute"
+    puts "#{name} is cuddly"
+    puts "#{name} is fluffy"
+    puts "They are #{age} years old."
+    #added age sentence 
+end 
+
+praise_cat("Misu", 8)
+#by adding 8 this is the second parameter, the code will reference this to add age value 
