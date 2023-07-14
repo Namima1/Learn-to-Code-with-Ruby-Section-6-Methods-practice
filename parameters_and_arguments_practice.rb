d00c6b17cdda5c72eb30f53b60568a5c4318904c
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-14 11:01:32 -0400

    Add files via upload

---
+++ parameters_and_arguments_practice.rb
@@ -0,0 +1,12 @@
+#parameter is name given to expected input 
+
+def praise_cat(name)
+    puts "#{name} is cute"
+    puts "#{name} is cuddly"
+    puts "#{name} is fluffy"
+end 
+
+praise_cat("Misu")
+
+#changed example in video from praise_person to praise_cat 
+#I defined the praise cat as Misu
