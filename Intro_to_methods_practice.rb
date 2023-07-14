a30aaa2eeda52a706fa352bf9a76626e50416fc6
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-14 11:01:10 -0400

    Add files via upload

---
+++ Intro_to_methods_practice.rb
@@ -0,0 +1,10 @@
+def introduce_myself 
+    puts "I am a women"
+    puts "I am confident"
+    puts "I am creative"
+end 
+
+#introduce_myself uses puts method to output text 
+#def is short for "define" 
+#end marks end of method definition 
+#without end ruby doesn't know what belongs to method definition and what belongs to file 
