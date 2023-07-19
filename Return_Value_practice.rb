8401ed5123f4adcd79a0b011fcf3f2c479b6d0da
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-19 11:05:37 -0400

    Add files via upload

---
+++ Return_Value_practice.rb
@@ -0,0 +1,10 @@
+def add_two_numbers(num1, num2)
+  puts "OK, I'm solving your math problem"
+  return num1 + num2 
+end
+
+result = add_two_numbers(1, 5)
+p result 
+
+#only returns one value 
+#the return value gets directly assigned 
