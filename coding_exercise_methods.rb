cd95e87bd10ba6dd56f1285f76d837f72212e990
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-19 11:07:31 -0400

    Add files via upload

---
+++ coding_exercise_methods.rb
@@ -0,0 +1,17 @@
+# Define a easy_money method that accepts no parameters 
+# and always returns the value 100.
+def easy_money 
+    return 100 
+end 
+
+puts easy_money 
+
+# Define a best_food_ever method that accepts 
+# no parameters and always returns the string "Sushi".
+def best_food_ever
+    return "Sushi"
+end 
+
+puts best_food_ever
+
+
